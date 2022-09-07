5d0b15d4b73079d2945cf5e6735de7361c99dd16
Alberto Núñez <albertonunezmaneiro@gmail.com>
2022-09-07 12:40:10 -0400

    Terminada la V2

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', PostController::class)->only(['index','show','destroy']);
+Route::apiResource('v1/posts', PostV1::class)->only(['index','show','destroy']);
+Route::apiResource('v2/posts', PostV2::class)->only(['index','show']);
